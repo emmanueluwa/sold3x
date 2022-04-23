86c9fc7f4c7ca727b3e3bd21a4f030a1bb183171
emmanueluwa <emmanuel@fulodev.com>
2022-04-23 15:03:52 +0100

    sold items in page succesfully fetched

--- prices.py
+++ prices.py
@@ -16,51 +16,61 @@ def get_driver():
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     return driver
 
+def get_sold_items(driver):
+    driver.get(ebay_sold_items_url)
+
+    #click advanced filter
+    advanced_link = driver.find_element(By.LINK_TEXT, "Advanced")
+    advanced_link.click()
+
+    # explicit wait, accept all for gdpr
+    try:
+        time.sleep(5)
+        accept_gdpr = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="gdpr-banner-accept"]')
+        ))
+        accept_gdpr.click()
+    except:
+        driver.quit()
+
+    #click sold items filter
+    time.sleep(5)
+    sold_filter_id = 'LH_Sold'
+    sold_link = driver.find_element(By.ID, sold_filter_id)
+    sold_link.click()
+
+    #search for product
+    searh_bar_id = '_nkw'
+    search_bar = driver.find_element(By.ID, searh_bar_id)
+    # make sure search bar is empty
+    search_bar.clear()
+    product = 'ps5'
+    search_bar.send_keys(product)
+    #press enter
+    search_bar.send_keys(Keys.RETURN)
+
+    #get the listed items
+    listed_items_class = 'sresult' 
+    try:
+        time.sleep(5)
+        found_items = driver.find_elements(By.CLASS_NAME, listed_items_class)
+    except:
+        driver.quit()
+        
+    return found_items
+
 #to run for this script only
 if __name__ == "__main__":
     print('Creatiing driver')
     driver = get_driver()
 
     print('fetching the page')
-    driver.get(ebay_sold_items_url)
     print('Page title', driver.title)
+    products = get_sold_items(driver)
+    print(f'Fetching sold items')
+    print(f'This is the number of items = {len(products)}')
 
-    #click advanced filter
-    advanced_link = driver.find_element(By.LINK_TEXT, "Advanced")
-    advanced_link.click()
-
-    # explicit wait
-    try:
-        time.sleep(5)
-        sold_filter = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="gdpr-banner-accept"]')
-        ))
-        sold_filter.click()
-
-       
-    except:
-        driver.quit()
-    
-    print('searching for item')
-    
-    #click sold items filter
-    time.sleep(5)
-    sold_link = driver.find_element(By.ID, "LH_Sold")
-    sold_link.click()
-
-    searh_bar_id = '_nkw'
-    search_bar = driver.find_element(By.ID, searh_bar_id)
-    # make sure search bar is empty
-    search_bar.clear()
-    search_bar.send_keys("ps5")
-    #press enter
-    search_bar.send_keys(Keys.RETURN)
-
-    #get the listed items
-    listed_items_class = 'sresult lvresult clearfix li' 
-    listed_items  = driver.find_element(By.CLASS_NAME, listed_items_class)
-    print(f'Found {len(listed_items)} items.')
-    # driver.quit()
+    driver.quit()
 
 
 
