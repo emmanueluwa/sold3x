7595e338003c93bc30f5d0801a39f888a70cd5a1
emmanueluwa <emmanuel@fulodev.com>
2022-04-23 08:14:15 +0100

    setting up driver and navigating to advanced filter page

--- prices.py
+++ prices.py
@@ -1,7 +1,12 @@
-import requests
+# https://selenium-python.readthedocs.io
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 ebay_sold_items_url = 'https://www.ebay.co.uk/'
 
@@ -19,7 +24,38 @@ if __name__ == "__main__":
     print('fetching the page')
     driver.get(ebay_sold_items_url)
     print('Page title', driver.title)
-    driver.quit()
+
+    #click advanced filter
+    advanced_link = driver.find_element(By.LINK_TEXT, "Advanced")
+    advanced_link.click()
+
+    # explicit wait
+    try:
+        time.sleep(5)
+        sold_filter = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="gdpr-banner-accept"]')
+        ))
+        sold_filter.click()
+
+       
+    except:
+        driver.quit()
+    
+    print('searching for item')
+    # search_bar = driver.find_element_by_id("gh-ac")
+    # # make sure search bar is empty
+    # search_bar.clear()
+    # search_bar.send_keys("ps5")
+    # #press enter
+    # search_bar.send_keys(Keys.RETURN)
+
+    
+    #click sold items filter
+    time.sleep(5)
+    sold_link = driver.find_element(By.NAME, "Sold listings")
+    sold_link.click()
+    # driver.quit()
+
 
 
 
